2ecbc332c32beed7881f0e00608a66de3e1acc2c
AiFahri <fahrinurilll@gmail.com>
2025-04-18 20:26:39 +0700

    feat: add onItemClick listener to JadwalAdapter for item selection

--- app/src/main/java/com/example/studentlife/adapter/JadwalAdapter.kt
+++ app/src/main/java/com/example/studentlife/adapter/JadwalAdapter.kt
@@ -2,6 +2,8 @@ package com.example.studentlife.adapter
 
 import android.app.AlertDialog
 import android.content.Context
+import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,7 +14,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.studentlife.R
 import com.example.studentlife.model.JadwalModel
 
-class JadwalAdapter(private val context: Context, private val data: MutableList<JadwalModel>, private val onDeleteClick: (Int) -> Unit) :
+class JadwalAdapter(private val context: Context, private val data: MutableList<JadwalModel>, private val onDeleteClick: (Int) -> Unit,  private val onItemClick: (Int) -> Unit)  :
     RecyclerView.Adapter<JadwalAdapter.ViewHolder>() {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -20,6 +22,7 @@ class JadwalAdapter(private val context: Context, private val data: MutableList<
         val tvMatkul: TextView = view.findViewById(R.id.tvMatkul)
         val tvHari: TextView = view.findViewById(R.id.tvHari)
         val tvJam: TextView = view.findViewById(R.id.tvJam)
+//        val imagePreview: ImageView = view.findViewById(R.id.imagePreview)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -35,8 +38,23 @@ class JadwalAdapter(private val context: Context, private val data: MutableList<
         holder.tvMatkul.text = "📖 ${item.namaMatkul}"
         holder.tvHari.text = item.hari
         holder.tvJam.text = item.jam
+//        Log.d("JadwalAdapter", "Image URI: ${item.imageUri}")
+//        if (!item.imageUri.isNullOrEmpty()) {
+//            try {
+//                holder.imagePreview.visibility = View.VISIBLE
+//                holder.imagePreview.setImageURI(Uri.parse(item.imageUri))
+//            } catch (e: Exception) {
+//                e.printStackTrace()
+//                holder.imagePreview.visibility = View.GONE
+//            }
+//        } else {
+//            holder.imagePreview.visibility = View.GONE
+//        }
         holder.btnDelete.setOnClickListener {
             onDeleteClick(position)
+        }
+        holder.itemView.setOnClickListener {
+            onItemClick(position)
         }
     }
 }
